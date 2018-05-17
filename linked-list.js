33deda97b73dc86d9eecada456b09a5c3846c3f1
leonardom249 <leonardo.megan@gmail.com>
2018-05-17 14:55:09 -0400

    added insert before, insert after, and insert at all with error handlers

--- linked-list.js
+++ linked-list.js
@@ -10,10 +10,60 @@ class _Node {
 class LinkedList {
   constructor(){
     this.head=null;
+
   }
 
   insertFirst(item){
     this.head= new _Node(item, this.head);
+  }
+
+  insertBefore(item, putBefore){
+    if(this.head === null){
+      return this.insertFirst(item);
+    }
+
+    if(this.head.value=== putBefore){
+      return this.insertFirst(item);
+    }
+  
+    let currNode= this.head;
+    let previousNode = this.head;
+  
+    while((currNode !== null) && (currNode.value !== putBefore)){
+      previousNode= currNode;
+      console.log(previousNode);
+      currNode = currNode.next;
+      console.log(currNode);
+    }
+    if(currNode === null){
+      console.log('Next item Not Found');
+      return;
+    }
+    previousNode.next= new _Node(item, currNode);
+  } 
+  
+
+  insertAfter(item, itemAfter){
+    if(this.head === null){
+      return this.insertFirst(item);
+    }
+  
+    if(this.head.value=== itemAfter){
+      return this.insertLast(item);
+    }
+    
+    let currNode= this.head;
+    let futureNode = this.head;
+    
+    while((currNode !== null) && (currNode.value !== itemAfter)){
+      currNode = currNode.next;
+      futureNode= currNode.next;
+    }
+    if(currNode === null){
+      console.log('Next item Not Found');
+      return;
+    }
+    currNode.next= new _Node(item, futureNode);
   }
 
   insertLast(item){
@@ -27,6 +77,29 @@ class LinkedList {
       }
       tempNode.next= new _Node(item, null);
     }
+  }
+
+  insertAt(item, position){
+    if(this.head === null || position ===0) {
+      return this.insertFirst(item);
+    }
+ 
+    let currentNode = this.head;
+    let prevNode = this.head;
+    let ticks =0;
+ 
+    while(ticks !== position){
+      if (currentNode.next === null && ticks === position-1){
+        return this.insertLast(item);
+      }
+      if (currentNode.next === null && ticks !== position-1){
+        throw new Error ('wrong');
+      }
+      prevNode=currentNode;
+      currentNode=currentNode.next;
+      ticks++;
+    }
+    prevNode.next= new _Node(item, currentNode);
   }
 
   find(item){
@@ -80,8 +153,16 @@ function main(){
   SLL.insertFirst('Apollo');
   SLL.insertLast('Boomer');
   SLL.insertLast('Helo');
+  SLL.insertLast('Husker');
+  SLL.insertLast('Starbuck');
+  SLL.insertLast('Tauhida');
 
+  //   SLL.remove('squirrel'); //Item not found :)
+
+  //   SLL.insertBefore('blah', 'Apollo');
+  SLL.insertAt('blah', 6);
+  console.log(JSON.stringify(SLL));
   return SLL;
 }
 
-console.log(main());
+main();
